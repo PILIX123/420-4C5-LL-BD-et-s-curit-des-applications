f3859e4767d4a68bbad8d91e0c6a3685d2bb921d
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2021-09-02 15:41:11 -0400

    update pour accomodation pour le back end local

--- film.js
+++ film.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('header'); 
 const section = document.querySelector('section');
 
-let requestURL = 'https://raw.githubusercontent.com/PILIX123/420-4C5-LL-BD-et-s-curit-des-applications/main/film.json';
+let requestURL = 'https://localhost:5001/films/Les%20Minions?JusteVedette=true&MaxVedette=2';
 
 request = new XMLHttpRequest(); //Ouverture de la requête
 
@@ -17,7 +17,9 @@ function maFonctionCallBack() {
 
     const Film = this.response;
     
-    maFonctionFilm(Film);
+    construireEntete(Film);
+    afficherVedette(Film);
+
 }
 function maFonctionFilm(film){
     const Films = film['Films'];
@@ -32,37 +34,39 @@ function maFonctionFilm(film){
 }
 function construireEntete(film) {
     const myH1 = document.createElement('h1');
-    myH1.textContent=film['Titre'];
+    myH1.textContent=film['titre'];
     header.appendChild(myH1);
 
-    const myPara1 = document.createElement('p');
-    myPara1.textContent = 'Directeur: ' + film['Directeur'] + ' // Auteur: ' + film['Auteur'];
-    header.appendChild(myPara1);
+    if(film['directeur']!=null){
+        const myPara1 = document.createElement('p');
+        myPara1.textContent = 'Directeur: ' + film['directeur'] + ' // Auteur: ' + film['auteur'];
+        header.appendChild(myPara1);
 
-    const myPara2 = document.createElement('p');
-    myPara2.textContent = 'Date: ' + film['Date'];
-    header.appendChild(myPara2);
+        const myPara2 = document.createElement('p');
+        myPara2.textContent = 'Date: ' + film['date'];
+        header.appendChild(myPara2);
 
-    const myPara3 = document.createElement('p');
-    myPara3.textContent = 'Budget: '+ film['Budget']+' $';
-    header.appendChild(myPara3);
-    const myPara4 = document.createElement('p');
-    myPara4.textContent = "Fin de semaine d'ouverture: " + film['Fin de semaine ouverture']+' $';
-    header.appendChild(myPara4);
-    const myPara5 = document.createElement('p');
-    myPara5.textContent = 'Total USA: '+ film['Total USA']+' $';
-    header.appendChild(myPara5);
-    const myPara6 = document.createElement('p');
-    myPara6.textContent =     'Total mondial: '+film['Total mondial']+' $';
-    header.appendChild(myPara6);
+        const myPara3 = document.createElement('p');
+        myPara3.textContent = 'Budget: '+ film['budget']+' $';
+        header.appendChild(myPara3);
+        const myPara4 = document.createElement('p');
+        myPara4.textContent = "Fin de semaine d'ouverture: " + film['fdsOpen']+' $';
+        header.appendChild(myPara4);
+        const myPara5 = document.createElement('p');
+        myPara5.textContent = 'Total USA: '+ film['usa']+' $';
+        header.appendChild(myPara5);
+        const myPara6 = document.createElement('p');
+        myPara6.textContent =     'Total mondial: '+film['mondial']+' $';
+        header.appendChild(myPara6);
 
-    const description = document.createElement('p');
-    description.textContent = 'Description: ' + film['Description'];
-    header.appendChild(description);
+        const description = document.createElement('p');
+        description.textContent = 'Description: ' + film['description'];
+        header.appendChild(description);
+    }
 }
 
 function afficherVedette(film) {
-    const Vedette = film['Vedette'];
+    const Vedette = film['vedettes'];
     
     for(var i =0; i< Vedette.length; i++){
         const myArticle = document.createElement('article');
@@ -70,9 +74,9 @@ function afficherVedette(film) {
         const myH2 = document.createElement('h2');
         const myPara1 = document.createElement('p');
 
-        myH2.textContent = Vedette[i].Nom;
-        myPara1.textContent = 'Personnage: ' + Vedette[i].Personnage;
-        Age.textContent = 'Age: ' + Vedette[i].Age;
+        myH2.textContent = Vedette[i].nom;
+        myPara1.textContent = 'Personnage: ' + Vedette[i].personnage;
+        Age.textContent = 'Age: ' + Vedette[i].age;
 
         myArticle.appendChild(myH2);
         myArticle.appendChild(myPara1);
